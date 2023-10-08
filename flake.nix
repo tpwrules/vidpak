a1ce04a1fa9878df657b34b05cb497a517be2a94
Thomas Watson <twatson52@icloud.com>
2023-10-07 22:38:08 -0500

    update nixpkgs and fix resulting nix develop issues

--- flake.nix
+++ flake.nix
@@ -9,7 +9,13 @@
       (flake-utils.lib.eachDefaultSystem (system: {
         packages.vidpak = nixpkgs.legacyPackages.${system}.python3Packages.callPackage ./. {};
 
+        # the dev shell needs pip so `nix develop` and `setuptoolsShellHook` can install the package in editable mode
+        devShells.vidpak = (self.packages.${system}.vidpak.overrideAttrs (o: {
+          nativeBuildInputs = (o.nativeBuildInputs or []) ++ [ nixpkgs.legacyPackages.${system}.python3Packages.pip ];
+        }));
+
         defaultPackage = self.packages.${system}.vidpak;
+        devShells.default = self.devShells.${system}.vidpak;
       }))
     ];
 }
